fbb96e1fd591338965fbe04ee88645eeede092c0
Anam0927 <anam.devdes27@gmail.com>
2020-11-20 14:20:11 +0530

    Fixed a bug that did not check for error on second submit click

--- JS/main.js
+++ JS/main.js
@@ -3,8 +3,10 @@ const emailInput = document.querySelector('#email');
 const submitButton = document.querySelector('.btn');
 
 submitButton.addEventListener('click', (e) => {
-    if (!regex.test(emailInput.value) && (!emailInput.classList.contains('error'))) {
-        emailInput.classList.add('error');
+    if (!regex.test(emailInput.value)) {
+        if (!emailInput.classList.contains('error')) {
+            emailInput.classList.add('error');
+        }
         e.preventDefault();
     } else {
         if (emailInput.classList.contains('error')) {
